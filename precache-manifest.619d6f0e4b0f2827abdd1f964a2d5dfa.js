588370d87bc6d198aee6ea597afae545ab681135
NewCoderAS <aryak.sathye@gmail.com>
2020-07-31 16:17:39 +0530

    Updates

---
+++ precache-manifest.619d6f0e4b0f2827abdd1f964a2d5dfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8963990f9653aa56636651dffce52a1",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "6297c855f0549cd6d1b9",
+    "url": "/CoronaTracker/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9f10d0b451e174a4b918",
+    "url": "/CoronaTracker/static/css/main.18bcb271.chunk.css"
+  },
+  {
+    "revision": "6297c855f0549cd6d1b9",
+    "url": "/CoronaTracker/static/js/2.15255ffa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaTracker/static/js/2.15255ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f10d0b451e174a4b918",
+    "url": "/CoronaTracker/static/js/main.c4e63950.chunk.js"
+  },
+  {
+    "revision": "2823c52ce22e9718ef03",
+    "url": "/CoronaTracker/static/js/runtime-main.a2d65459.js"
+  }
+]);
